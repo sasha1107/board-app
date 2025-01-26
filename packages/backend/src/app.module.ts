94db349cae129fd7833b2a77f6ffc6c4fceabdf1
sasha1107 <sasha1107@naver.com>
2025-01-27 00:08:04 +0900

    feat: boards controller 추가

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { BoardsModule } from "./boards/boards.module";
+import { BoardsModule } from "@/boards/boards.module";
 
 @Module({
   imports: [BoardsModule],
