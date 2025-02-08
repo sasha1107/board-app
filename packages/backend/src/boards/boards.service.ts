6ecf765692a607ca40e95d77a73937ba59619e51
sasha1107 <sasha1107@naver.com>
2025-02-08 16:50:34 +0900

    feat: [API] GET '/boards/:id' 예외 처리

--- packages/backend/src/boards/boards.service.ts
+++ packages/backend/src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { BoardStatus, type Board } from "./board.model";
 import { v1 as uuid } from "uuid";
 import type { CreateBoardDto } from "./dto/create-board.dto";
@@ -25,7 +25,11 @@ export class BoardsService {
   }
 
   getBoardById(id: string) {
-    return this.boards.find((board) => board.id === id);
+    const found = this.boards.find((board) => board.id === id);
+    if (!found) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+    return found;
   }
 
   deleteBoard(id: string) {
