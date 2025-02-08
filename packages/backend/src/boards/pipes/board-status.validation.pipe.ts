72aa92a3e2c76d0e769e31a5189b74a496bde704
sasha1107 <sasha1107@naver.com>
2025-02-08 18:05:08 +0900

    feat: [API] PATCH '/boards/:id/status' 예외처리

---
+++ packages/backend/src/boards/pipes/board-status.validation.pipe.ts
@@ -0,0 +1,21 @@
+import { PipeTransform, BadRequestException } from "@nestjs/common";
+import { BoardStatus } from "../board.model";
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  // 클래스 외부에서 액세스할 수 있지먄 해당 값은 변경할 수 없다.
+  readonly StatusOptions: string[] = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
+
+  transform(value: unknown) {
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`Invalid status`);
+    }
+    return value;
+  }
+
+  private isStatusValid(status: unknown) {
+    if (typeof status !== "string") {
+      return false;
+    }
+    return this.StatusOptions.find((s) => s === status.toUpperCase());
+  }
+}
