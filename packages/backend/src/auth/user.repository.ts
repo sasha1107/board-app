4d20860b38b36ba768a84d50532f5f9a95714833
sasha1107 <sasha1107@naver.com>
2025-02-22 20:30:41 +0900

    feat: [API] POST '/auth/signup' - 'username' unique 한 entity로 설정

--- packages/backend/src/auth/user.repository.ts
+++ packages/backend/src/auth/user.repository.ts
@@ -1,6 +1,13 @@
 import { Repository, DataSource } from "typeorm";
 import { User } from "@/auth/user.entity";
 import { AuthCredentialDto } from "@/auth/dto/auth-credential.dto";
+import {
+  ConflictException,
+  InternalServerErrorException,
+  Injectable,
+} from "@nestjs/common";
+
+@Injectable()
 export class UserRepository extends Repository<User> {
   constructor(dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
@@ -9,6 +16,16 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto) {
     const { username, password } = authCredentialDto;
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (error.code === "23505") {
+        throw new ConflictException("Existing username");
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
